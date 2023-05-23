d29a475a10d318c80f417f1bf4801dbb65256c96
delwerhossain <delwerhossain006@gmail.com>
2023-05-23 11:03:03 +0100

    remove extra menu

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,10 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layouts from "../Layout/Layouts";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import Home from "../pages/Home/Home";
-import Contact from "../pages/Contact/Contact";
-import About from "../pages/About/About";
-import Courses from "../pages/Courses/Courses";
-import Blogs from "../pages/Blogs/Blogs";
+
 
 export const router = createBrowserRouter([
     {
@@ -16,22 +13,6 @@ export const router = createBrowserRouter([
         {
           path: "/",
           element: <Home />,
-        },
-        {
-          path: "contact",
-          element: <Contact />,
-        },
-        {
-          path: "about",
-          element: <About />,
-        },
-        {
-          path: "courses",
-          element: <Courses />,
-        },
-        {
-          path: "blogs",
-          element: <Blogs />,
         },
       ],
     },
