4eab5bfb65f374cdb94582949f6c2f72fe13c4a9
delwerhossain <delwerhossain006@gmail.com>
2023-06-06 13:32:47 +0100

    Private route add

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import { useContext } from "react";
+import { Navigate, useLocation } from "react-router";
+import { AuthContext } from "../provider/AuthProvider";
 
-const PrivateRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+
+  if (loading) {
+    return <progress className="progress w-56"></progress>;
+  }
+
+  if (user) {
+    return children;
+  }
+  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
 };
 
 export default PrivateRoute;
